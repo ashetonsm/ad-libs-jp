34c0500491e61b60592e666f1dfd7dfc17d36a1b
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-03-11 20:57:41 +0900

    Added an actual story and the FreeInput component.

--- src/components/ScenarioOne.tsx
+++ src/components/ScenarioOne.tsx
@@ -1,9 +1,41 @@
+import FreeInput from "./FreeInput"
+
 function ScenarioOne() {
 
   return (
     <div>
-        <h1>I am ScenarioOne</h1>
-    </div>
+      <h1>The Town Legend</h1>
+
+      <p>
+        Once upon a time, a wanderer named <FreeInput wordType="Your Name" />
+      </p>
+      <p>
+        visited a town called <FreeInput wordType="Proper Noun" />.
+      </p>
+      <p>
+        Suddenly, <FreeInput wordType="Number" /> <FreeInput wordType="Adjective" /> monsters attacked the town!
+      </p>
+      <p>
+        Although <FreeInput wordType="Your Name" /> was <FreeInput wordType="Adjective" />, <FreeInput wordType="Pronoun" /> fought them off.
+      </p>
+      <p>
+        The town was <FreeInput wordType="Past-Tense Verb" />.
+      </p>
+      <p>
+        The king said "The people of our town will <FreeInput wordType="Frequency Adverb" />
+      </p>
+      <p>
+        remember how you <FreeInput wordType="Adverb" /> you <FreeInput wordType="Past-Tense Verb" />."
+      </p>
+      <p>
+        He presented <FreeInput wordType="Your Name" /> with a <FreeInput wordType="Noun" />.
+      </p>
+      <p>
+        To this day, it is said that <FreeInput wordType="Your Name" /> <FreeInput wordType="Past-Tense Verb" /> it.
+      </p>
+
+      <p>THE END.</p>
+    </div >
   )
 }
 
